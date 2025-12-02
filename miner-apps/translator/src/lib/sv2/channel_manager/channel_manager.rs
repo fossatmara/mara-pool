38c43e47a34f97cc17b3fcd92fdb9ba25299636f
Eric Price <eric@navier.com>
2025-12-02 14:55:08 -0500

    fix(clippy): Use infallible conversion in translator test

--- miner-apps/translator/src/lib/sv2/channel_manager/channel_manager.rs
+++ miner-apps/translator/src/lib/sv2/channel_manager/channel_manager.rs
@@ -849,7 +849,7 @@ mod tests {
         let update_channel = UpdateChannel {
             channel_id: 1,
             nominal_hash_rate: 2000.0,
-            maximum_target: [0xFFu8; 32].try_into().unwrap(),
+            maximum_target: [0xFFu8; 32].into(),
         };
 
         // Test that the message can be handled
